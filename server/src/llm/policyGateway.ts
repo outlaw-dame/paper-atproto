d69c8113f8e8261d57eba6ee05f4fc3f159f6758
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-04-30 19:00:59 -0400

    feat: allow composer classifier policy task

--- server/src/llm/policyGateway.ts
+++ server/src/llm/policyGateway.ts
@@ -7,6 +7,7 @@ export type LlmPolicyTask =
   | 'media'
   | 'searchStory'
   | 'composerGuidance'
+  | 'composerClassifier'
   | 'premiumDeep'
   | 'verificationGrounding'
   | 'localTextGeneration'
